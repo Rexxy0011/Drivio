52ada4273a5c3f7f6da5297a0a6c1c02155be39c
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-02-16 10:36:07 +0100

    added the signup and login controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,59 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Generate JWT token
+const generateToken = (userId) => {
+  const payload = { userId };
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password || password.length < 6) {
+      return res.json({
+        success: false,
+        message: "Please fill all the fields",
+      });
+    }
+
+    // check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    const token = generateToken(user._id.toString());
+    return res.json({ success: true, token });
+  } catch (error) {
+    console.error(error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// Login user
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user._id.toString());
+    return res.json({ success: true, token });
+  } catch (error) {
+    console.error(error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
