6438d9e0fbbfecd8f2c2fbf7a69ce44f41b74691
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-02-18 22:17:52 +0100

    added the models for the car owners

--- server/models/Car.js
+++ server/models/Car.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose";
 const { objectId } = mongoose.Schema.Types;
 
-const carSchema = new mongoose.Schema({
-  owner: { type: objectId, ref: "User", required: true },
-});
+const carSchema = new mongoose.Schema(
+  {
+    owner: { type: objectId, ref: "User", required: true },
+    model: { type: String, required: true },
+    image: { type: String, required: true },
+    year: { type: Number, required: true },
+    category: { type: String, required: true },
+    seating_capacity: { type: Number, required: true },
+    fuel_type: { type: String, required: true },
+    transmission: { type: String, required: true },
+    pricePerDay: { type: Number, required: true },
+    desciption: { type: String, required: true },
+    isAvailable: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+const Car = mongoose.model("Car", carSchema);
+
+export default Car;
