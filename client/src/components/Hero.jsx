8f4353355f8baab6b7734b81f67600ba33b805d9
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-02-28 10:03:06 +0100

    added the search url in hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { assets, cityList } from "../assets/assets";
 import { useState } from "react";
+import { useAppContext } from "../context/AppContext";
 
 const Hero = () => {
   const [pickupLocation, setPickupLocation] = useState("");
+  const { pickupDate, setPickupDate, returnDate, setReturnDate, navigate } =
+    useAppContext();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(
+      `/cars?pickupLocation=${pickupLocation}&pickupDate=${pickupDate}&returnDate=${returnDate}`
+    );
+  };
+
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-14  text-center">
       <h1 className="text-4xl md:text-5xl font-semibold">
@@ -19,7 +30,10 @@ const Hero = () => {
         </span>
       </h1>
 
-      <form className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
+      <form
+        onSubmit={handleSearch}
+        className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
+      >
         <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
           <div className="flex flex-col items-start gap-2">
             <select
@@ -28,17 +42,22 @@ const Hero = () => {
               onChange={(e) => setPickupLocation(e.target.value)}
             >
               <option value="">Pickup Location</option>
-              {cityList.map((city) => (
-                <option value={city}>{city}</option>
+              {cityList.map((city, i) => (
+                <option key={i} value={city}>
+                  {city}
+                </option>
               ))}
             </select>
             <p className="px-1 text-sm text-gray-500">
               {pickupLocation ? pickupLocation : "Please select location"}
             </p>
           </div>
+
           <div className="flex flex-col items-start gap-2">
             <label htmlFor="pickup-date">Pick-up Date</label>
             <input
+              value={pickupDate}
+              onChange={(e) => setPickupDate(e.target.value)}
               type="date"
               id="pickup-date"
               min={new Date().toISOString().split("T")[0]}
@@ -46,9 +65,12 @@ const Hero = () => {
               required
             />
           </div>
+
           <div className="flex flex-col items-start gap-2">
             <label htmlFor="return-date">Return Date</label>
             <input
+              value={returnDate}
+              onChange={(e) => setReturnDate(e.target.value)}
               type="date"
               id="return-date"
               className="text-sm text-gray-500"
@@ -56,7 +78,8 @@ const Hero = () => {
             />
           </div>
         </div>
-        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover: bg-primary-dull text-white rounded-full cursor-pointer">
+
+        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
           <img
             src={assets.search_icon}
             alt="search"
@@ -65,8 +88,10 @@ const Hero = () => {
           Search
         </button>
       </form>
+
       <img src={assets.main_car} alt="" className="max-h-74" />
     </div>
   );
 };
+
 export default Hero;
