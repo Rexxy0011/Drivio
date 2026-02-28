936a495da1f529dcb76b865a415c98c9e9c5cf5c
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-02-28 10:44:31 +0100

    added the search filter

--- client/src/Pages/Cars.jsx
+++ client/src/Pages/Cars.jsx
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Title from "../components/Title";
-import { assets, dummyCarData } from "../assets/assets";
-import { useState } from "react";
+import { assets } from "../assets/assets";
 import CarCard from "../components/CarCard";
+import { useSearchParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useAppContext } from "../context/AppContext";
 
 const Cars = () => {
+  // getting search params from url
+  const [searchParams] = useSearchParams();
+  const pickupLocation = searchParams.get("pickupLocation");
+  const pickupDate = searchParams.get("pickupDate");
+  const returnDate = searchParams.get("returnDate");
+
+  const { cars, axios } = useAppContext();
+
   const [input, setInput] = useState("");
+  const [baseCars, setBaseCars] = useState([]);
+  const [filteredCars, setFilteredCars] = useState([]);
+
+  const isSearchData = pickupLocation && pickupDate && returnDate;
+
+  const applyFilter = () => {
+    if (input === "") {
+      setFilteredCars(baseCars);
+      return;
+    }
+
+    const filtered = baseCars.filter((car) => {
+      return (
+        car.brand.toLowerCase().includes(input.toLowerCase()) ||
+        car.model.toLowerCase().includes(input.toLowerCase()) ||
+        car.transmission.toLowerCase().includes(input.toLowerCase())
+      );
+    });
+
+    setFilteredCars(filtered);
+  };
+
+  const searchCarAvailability = async () => {
+    try {
+      const { data } = await axios.post("/api/bookings/check-availability", {
+        location: pickupLocation,
+        pickupDate,
+        returnDate,
+      });
+
+      if (data.success) {
+        setBaseCars(data.cars);
+        setFilteredCars(data.cars);
+
+        if (data.cars.length === 0) {
+          toast.error("No cars available");
+        }
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error?.response?.data?.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (isSearchData) {
+      searchCarAvailability();
+    } else {
+      setBaseCars(cars);
+      setFilteredCars(cars);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSearchData, cars]);
+
+  useEffect(() => {
+    applyFilter();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [input, baseCars]);
+
   return (
     <div>
       <div className="flex flex-col items-center py-20 bg-light max-md:px-4">
@@ -22,17 +93,18 @@ const Cars = () => {
             placeholder="Search make, model, or features..."
             className="w-full h-full outline-none text-gray-500"
           />
-
           <img src={assets.filter_icon} alt="" className="w-4.5 h-4.5 ml-2" />
         </div>
       </div>
+
       <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-10">
         <p className="text-gray-500 xl:px-20 max-w-7xl mx-auto">
-          Showing {dummyCarData.length} Cars
+          Showing {filteredCars.length} Cars
         </p>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto">
-          {dummyCarData.map((car, index) => (
-            <div key={index}>
+          {filteredCars.map((car) => (
+            <div key={car._id}>
               <CarCard car={car} />
             </div>
           ))}
